27c3d7e925c62ce218df8c195136e4188fa8d0af
Caleb Wolin <caleb.wolin@gmail.com>
2022-11-16 11:58:34 -0700

    Milestone 5

---
+++ Milestone4/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"app","l":"Armor"},{"p":"app","l":"Health"},{"p":"app","l":"InventoryManager"},{"p":"app","l":"Product"},{"p":"app","l":"ShoppingCart"},{"p":"app","l":"StoreFront"},{"p":"app","l":"Weapon"}];updateSearchResults();
